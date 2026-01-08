68c07f8415e2af5af8cf3330a494ea2528c144a6
Carles Bou <cbou@zat.net>
2026-01-08 20:46:37 +0100

    entrega ej6b3

--- 6b/ej6b3.py
+++ 6b/ej6b3.py
@@ -64,24 +64,22 @@ from typing import List
 
 
 def isprime(num):
-    #Find the error and rewrite the correct code. 
-    if num < 1:
+    if num < 2:
         return False
     for i in range(2, num):
-        if num % i != 0:
+        if num % i == 0:
             return False
     return True
 
 
-def check_primes(nums: List[int]) -> List[bool]:
-    #Find the error and rewrite the correct code. 
+def check_primes(nums: List[int]) -> List[bool]:  
     results = []
-    for i in range(1, len(nums)):
-        results.append(isprime(i))
+    for i in range(0, len(nums)):
+        results.append(isprime(nums[i]))
     return results
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-#numbers_list = [1, 5, 11, 12, 13, 14, 15]
-#print(check_primes(numbers_list))
+# numbers_list = [1, 5, 11, 12, 13, 14, 15]
+# print(check_primes(numbers_list))
